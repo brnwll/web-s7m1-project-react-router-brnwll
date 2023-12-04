900e1f1b3b01ef2ccda9177e57311b23d07b50d2
Brian Howell <brianhowell@pm.me>
2023-12-04 13:46:29 -0500

    Refactor duplicated MovieDetails and Movie JSX into MovieCard component

--- src/Movies/MovieList.js
+++ src/Movies/MovieList.js
@@ -1,30 +1,18 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
 export default function MovieList(props) {
+  const navigate = useNavigate();
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <MovieCard
+          key={movie.id} 
+          movie={movie} 
+          handleClick={() => navigate(`/movies/${movie.id}`)} 
+        />
       ))}
-    </div>
-  );
-}
-
-function MovieDetails(props) {
-  const { id, title, director, metascore } = props.movie;
-  const navigate = useNavigate()
-  const handleClick = id => navigate(`/movies/${id}`)
-
-  return (
-    <div className="movie-card" onClick={() => handleClick(id)}>
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
     </div>
   );
 }
